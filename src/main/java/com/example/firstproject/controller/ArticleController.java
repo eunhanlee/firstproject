d29515520954de63be868961b087b9f5eb6b6004
eunhanlee58 <eunhanlee58@gmail.com>
2024-01-25 20:14:40 -0500

    add post function with dto using postMapping

---
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -0,0 +1,22 @@
+package com.example.firstproject.controller;
+
+import com.example.firstproject.dto.ArticleForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+    @GetMapping("/article/new")
+    public String newArticleForm() {
+        return "article/new";
+    }
+
+    @PostMapping("/article/create")
+    public String createArticle(ArticleForm form){
+        System.out.println(form.toString());
+        return "";
+    }
+
+
+}
