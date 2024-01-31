8483dadf4a8d9de7f7963bc936d87ca8fbb008b3
eunhanlee58 <eunhanlee58@gmail.com>
2024-01-31 16:09:06 -0500

    add reading for article/{id}

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,7 +6,9 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -14,19 +16,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+
     @GetMapping("/article/new")
     public String newArticleForm() {
         return "article/new";
     }
 
     @PostMapping("/article/create")
-    public String createArticle(ArticleForm form){
+    public String createArticle(ArticleForm form) {
         log.info(form.toString());
-        Article article  = form.toEntity();
+        Article article = form.toEntity();
         log.info(article.toString());
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
         return "";
+    }
+
+    @GetMapping("/article/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        log.info("id = " + id);
+//        Optional<Article> articleEntity = articleRepository.findById(id);
+        Article arricleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", arricleEntity);
+
+
+
+        return "article/show";
     }
 
 
