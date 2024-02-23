462879ee65d5104d5d3bace4c037de19baff75e8
eunhanlee58 <eunhanlee58@gmail.com>
2024-02-23 17:52:29 -0500

    fix article and add @GeneratedValue(strategy = GenerationType.IDENTITY)
    to autp create the id

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO article(id, title, content) VALUES(1, 'aaaaa', '1111');
-INSERT INTO article(id, title, content) VALUES(2, 'bbbbb', '2222');
-INSERT INTO article(id, title, content) VALUES(3, 'cccc', '3333');
+INSERT INTO article(title, content) VALUES('aaaaa', '1111');
+INSERT INTO article(title, content) VALUES('bbbbb', '2222');
+INSERT INTO article(title, content) VALUES('cccc', '3333');
